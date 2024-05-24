036cc5d5fbe85579d68d27c5113ca60dba21239d
pavlo <pavlolavryk@gmail.com>
2024-05-24 22:33:02 +0300

    Initial commit

---
+++ src/main/java/org/example/CalculatorTest.java
@@ -0,0 +1,26 @@
+package org.example;
+
+// Файл: CalculatorTest.java
+
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class CalculatorTest {
+
+    @Test
+    public void testAdd() {
+        Calculator calculator = new Calculator();
+        int result = calculator.add(3, 5);
+        assertEquals(8, result);
+    }
+
+    @Test
+    public void testSubtract() {
+        Calculator calculator = new Calculator();
+        int result = calculator.subtract(10, 4);
+        assertEquals(6, result);
+    }
+}
+
